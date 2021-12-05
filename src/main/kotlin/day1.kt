01b6efd1870f337a3f3efdb6e89a311d3518298e
Jesper Evertsson <jespa_evertsson@hotmail.com>
2021-12-05 14:46:00 +0100

    Clean up solotion for day 1

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,9 +1,28 @@
 fun day1part1() {
-    val lines: List<String> = readFile("day01.txt")
-    val numbers = lines.map {
-        Integer.valueOf(it)
+    val answer = findAnswer(getInput())
+
+    println("1a: $answer")
+}
+
+fun day1part2() {
+    val numbers = getInput()
+    val windows: MutableList<Int> = mutableListOf()
+
+    for (i in 0..numbers.size - 3) {
+        windows.add(numbers[i] + numbers[i + 1] + numbers[i + 2])
     }
 
+    val answer = findAnswer(windows)
+
+    println("1b: $answer")
+}
+
+fun getInput(): List<Int> {
+    val lines: List<String> = readFile("day01.txt")
+    return lines.map { Integer.valueOf(it) }
+}
+
+fun findAnswer(numbers: List<Int>): Int {
     var answer = 0
     var prevNumber: Int? = null
     for (number in numbers) {
@@ -19,35 +38,5 @@ fun day1part1() {
         prevNumber = number
     }
 
-    println("1a: $answer")
-}
-
-fun day1part2() {
-    val lines: List<String> = readFile("day01.txt")
-    val numbers = lines.map {
-        Integer.valueOf(it)
-    }
-    val windows: MutableList<Int> = mutableListOf()
-
-    for (i in 0..numbers.size - 3) {
-        windows.add(numbers[i] + numbers[i + 1] + numbers[i + 2])
-    }
-
-    var answer = 0
-    var prevWindow: Int? = null
-
-    for (window in windows) {
-        if (prevWindow == null) {
-            prevWindow = window
-            continue
-        }
-
-        if (prevWindow < window) {
-            answer++
-        }
-
-        prevWindow = window
-    }
-
-    println("1b: $answer")
+    return answer
 }
