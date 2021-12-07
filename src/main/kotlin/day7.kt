1ca8bd05966245eb03515b404e09558a97487a39
Jesper Evertsson <jespa_evertsson@hotmail.com>
2021-12-07 19:07:40 +0100

    add solutions for day 7

---
+++ src/main/kotlin/day7.kt
@@ -0,0 +1,68 @@
+import kotlin.math.abs
+
+fun day7() {
+    val lines: List<String> = readFile("day07.txt")
+
+    day7part1(lines)
+    day7part2(lines)
+}
+
+fun day7part1(lines: List<String>) {
+    val positions = lines[0].split(",").map { Integer.valueOf(it) }
+    val min = positions.minOrNull() ?: 0
+    val max = positions.maxOrNull() ?: 0
+
+    var lowestFuelCost = Int.MAX_VALUE
+
+    for (i in min..max) {
+        var cost = 0
+        positions.forEach {
+            cost += abs(it - i)
+        }
+
+        if (cost < lowestFuelCost) {
+            lowestFuelCost = cost
+        }
+    }
+
+    val answer = lowestFuelCost
+
+    println("7a: $answer")
+}
+
+fun day7part2(lines: List<String>) {
+    val positions = lines[0].split(",").map { Integer.valueOf(it) }
+    val min = positions.minOrNull() ?: 0
+    val max = positions.maxOrNull() ?: 0
+
+    var lowestFuelCost = Int.MAX_VALUE
+
+    for (i in min..max) {
+        var cost = 0
+        positions.forEach {
+            cost += calculateFuelCost(it, i)
+        }
+
+        if (cost < lowestFuelCost) {
+            lowestFuelCost = cost
+        }
+    }
+
+    val answer = lowestFuelCost
+
+    println("7b: $answer")
+}
+
+fun calculateFuelCost(startPosition: Int, goalPosition: Int): Int {
+    var currentCost = 1
+    var totalCost = 0
+    var distanceMoved = 0
+    val distance = abs(startPosition - goalPosition)
+
+    while (distanceMoved < distance) {
+        distanceMoved++
+        totalCost += currentCost++
+    }
+
+    return totalCost
+}
