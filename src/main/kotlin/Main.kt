38ba5c48baffdf2ef3d1ac485c1b34920c9ec1cc
Jesper Evertsson <jespa_evertsson@hotmail.com>
2021-12-10 23:25:03 +0100

    Add support for running multiple iterations of all solutions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,35 @@ import kotlin.system.measureTimeMillis
 suspend fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    // println("Program arguments: ${args.joinToString()}")
+   //  println("Program arguments: ${args.joinToString()}")
 
-    println("solving")
+    /*val intArgs: List<Int> = args.map { Integer.valueOf(it) }
+
+    val iterations = intArgs[0]
+    val daysToSolve = intArgs.filterIndexed { i, _ ->
+        i > 0
+    }
+
+    for (i in 0 until iterations) {
+        println("Solving")
+        daysToSolve.forEach {
+            when (it) {
+                1 -> day1()
+                2 -> day2()
+                3 -> day3()
+                4 -> day4()
+                5 -> day5()
+                6 -> day6()
+                7 -> day7()
+                8 -> day8()
+                9 -> day9()
+                10 -> day10()
+            }
+        }
+        println("Done")
+    }*/
+
+    println("Solving")
     println("---- Took ${measureTimeMillis{ day1() }} ms to execute ----")
     println("---- Took ${measureTimeMillis{ day2() }} ms to execute ----")
     println("---- Took ${measureTimeMillis{ day3() }} ms to execute ----")
@@ -17,6 +43,7 @@ suspend fun main(args: Array<String>) {
     println("---- Took ${measureTimeMillis{ day8() }} ms to execute ----")
     println("---- Took ${measureTimeMillis{ day9() }} ms to execute ----")
     println("---- Took ${measureTimeMillis{ day10() }} ms to execute ----")
+    println("Done")
 }
 
 fun readFile(fileName: String): List<String> {
