93d0e372f23b95e57b821a36e40e1bc9934bea27
Jesper Evertsson <jespa_evertsson@hotmail.com>
2021-12-29 23:32:56 +0100

    Update day 18 code to support numbers larger than 9, still need to fix some cases

--- src/main/kotlin/day18.kt
+++ src/main/kotlin/day18.kt
@@ -1,6 +1,5 @@
 import kotlin.math.ceil
 import kotlin.math.floor
-import kotlin.math.roundToInt
 
 fun day18() {
     val lines: List<String> = readFile("day18.txt")
@@ -10,11 +9,8 @@ fun day18() {
 }
 
 fun day18part1(lines: List<String>) {
-    val test = addSnailFishNumber(lines[0], lines[1])
 
-    val test2 = explodeSnailFishNumber("[7,[6,[5,[4,[3,2]]]]]")
-
-    val answer = test2
+    val answer = sumListOfSnailFishNumbers(lines)
 
     println("18a: $answer")
 }
@@ -23,6 +19,22 @@ fun day18part2(lines: List<String>) {
     val answer = "TBD"
 
     println("18b: $answer")
+}
+
+fun sumListOfSnailFishNumbers(lines: List<String>): String {
+    var finalSum = ""
+
+    lines.forEachIndexed { index, it ->
+        if (index < lines.size - 1) {
+            if (finalSum.isEmpty()) {
+                finalSum = addSnailFishNumber(lines[index], lines[index + 1])
+            } else {
+                finalSum = addSnailFishNumber(finalSum, lines[index + 1])
+            }
+        }
+    }
+
+    return finalSum
 }
 
 fun addSnailFishNumber(left: String, right: String): String {
@@ -92,36 +104,30 @@ fun explodeSnailFishNumber(number: String): String {
         return number
     }
 
-    val leftSide: String
-    val rightSide: String
+    val endExplodeIndex = number.indexOf(']', explodeIndex) + 1
+
+    val leftValue = Integer.valueOf(number.substring(explodeIndex, number.indexOf(',', explodeIndex)))
+    val rightValue = Integer.valueOf(number.substring(number.indexOf(',', explodeIndex) + 1, endExplodeIndex - 1))
+
+    var leftSide  = number.substring(0..explodeIndex - 2)
+    var rightSide = number.substring(endExplodeIndex until number.length)
 
     val leftExplodeIndex = findLastNumberIndex(number.substring(0 until explodeIndex))
-    leftSide = if (leftExplodeIndex > 0) {
-        val newNumber = Integer.valueOf(number[leftExplodeIndex].toString()) + Integer.valueOf(number[explodeIndex].toString())
-        if (number[explodeIndex - 3].isDigit()) {
-            number.substring(0..explodeIndex - 2).replaceRange(leftExplodeIndex..leftExplodeIndex, newNumber.toString())
-        } else {
-            (number.substring(0..explodeIndex - 2) + "0,").replaceRange(leftExplodeIndex..leftExplodeIndex, newNumber.toString())
-        }
-    } else {
-        number.substring(0..explodeIndex - 2) + "0,"
+    val rightExplodeIndex = findFirstNumberIndex(number.substring(endExplodeIndex until number.length))
+
+    if (leftExplodeIndex > 0) {
+        //TODO: Support values larger than 9
+        val newNumber = Integer.valueOf(number[leftExplodeIndex].toString()) + leftValue
+        leftSide = leftSide.replaceRange(leftExplodeIndex..leftExplodeIndex, newNumber.toString())
     }
 
-    val rightExplodeIndex = findFirstNumberIndex(number.substring(explodeIndex + 3 until number.length), explodeIndex + 3)
-    rightSide = if (rightExplodeIndex > 0) {
-        val newNumber = Integer.valueOf(number[rightExplodeIndex].toString()) + Integer.valueOf(number[explodeIndex + 2].toString())
-        if (number[explodeIndex + 5].isDigit()) {
-            number.substring(explodeIndex + 5 until number.length)
-                .replaceRange(rightExplodeIndex - explodeIndex - 5 until rightExplodeIndex - explodeIndex - 4, newNumber.toString())
-        } else {
-            val temp = number.substring(explodeIndex + 4 until number.length)
-            "0" + temp.replaceRange(rightExplodeIndex - explodeIndex - 4 until rightExplodeIndex - explodeIndex - 3, newNumber.toString())
-        }
-    } else {
-        "0" + number.substring(explodeIndex + 4 until number.length)
+    if (rightExplodeIndex > 0) {
+        //TODO: Support values larger than 9
+        val newNumber = Integer.valueOf(rightSide[rightExplodeIndex].toString()) + rightValue
+        rightSide = rightSide.replaceRange(rightExplodeIndex..rightExplodeIndex, newNumber.toString())
     }
 
-    return leftSide + rightSide
+    return leftSide + "0" + rightSide
 }
 
 fun findLastNumberIndex(number: String): Int {
@@ -136,10 +142,10 @@ fun findLastNumberIndex(number: String): Int {
     return lastNumberIndex
 }
 
-fun findFirstNumberIndex(number: String, offset: Int): Int {
+fun findFirstNumberIndex(number: String): Int {
     number.forEachIndexed { index, it ->
         if (it.isDigit()) {
-            return index + offset
+            return index
         }
     }
 
--- src/test/kotlin/Day18KtTest.kt
+++ src/test/kotlin/Day18KtTest.kt
@@ -30,6 +30,16 @@ internal class Day18KtTest {
     }
 
     @Test
+    fun testExampleExplode() {
+        assertEquals("[[[[0,7],4],[15,[0,13]]],[1,1]]", explodeSnailFishNumber("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]"))
+    }
+
+    @Test
+    fun testProblematicExplode() {
+        assertEquals("[[[[4,0],[5,4]],[[7,7],[6,0]]],[17,[[11,9],[11,0]]]]", explodeSnailFishNumber("[[[[4,0],[5,4]],[[7,7],[0,[6,7]]]],[10,[[11,9],[11,0]]]]"))
+    }
+
+    @Test
     fun testSplitNumber() {
         assertEquals("[7,8]", splitNumber(15))
     }
@@ -42,5 +52,74 @@ internal class Day18KtTest {
     @Test
     fun testAddWithReduce() {
         assertEquals("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", addSnailFishNumber("[[[[4,3],4],4],[7,[[8,4],9]]]", "[1,1]"))
+    }
+
+    @Test
+    fun testAddWithReduceExample() {
+        assertEquals("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", addSnailFishNumber(
+            "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+            "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
+        ))
+    }
+
+    @Test
+    fun testSumListOfSnailFishNumbers() {
+        assertEquals(
+            "[[[[1,1],[2,2]],[3,3]],[4,4]]",
+            sumListOfSnailFishNumbers(listOf(
+                "[1,1]",
+                "[2,2]",
+                "[3,3]",
+                "[4,4]"
+            ))
+        )
+    }
+
+    @Test
+    fun testSumListOfSnailFishNumbersThatRequiresReduce() {
+        assertEquals(
+            "[[[[3,0],[5,3]],[4,4]],[5,5]]",
+            sumListOfSnailFishNumbers(listOf(
+                "[1,1]",
+                "[2,2]",
+                "[3,3]",
+                "[4,4]",
+                "[5,5]"
+            ))
+        )
+    }
+
+    @Test
+    fun testSumListOfSnailFishNumbersThatRequiresSeveralReduces() {
+        assertEquals(
+            "[[[[5,0],[7,4]],[5,5]],[6,6]]",
+            sumListOfSnailFishNumbers(listOf(
+                "[1,1]",
+                "[2,2]",
+                "[3,3]",
+                "[4,4]",
+                "[5,5]",
+                "[6,6]"
+            ))
+        )
+    }
+
+    @Test
+    fun testSumListOfSnailFishNumbersLargeExample() {
+        assertEquals(
+            "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]",
+            sumListOfSnailFishNumbers(listOf(
+                "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+                "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+                "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+                "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+                "[7,[5,[[3,8],[1,4]]]]",
+                "[[2,[2,2]],[8,[8,1]]]",
+                "[2,9]",
+                "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+                "[[[5,[7,4]],7],1]",
+                "[[[[4,2],2],6],[8,7]]"
+            ))
+        )
     }
 }
